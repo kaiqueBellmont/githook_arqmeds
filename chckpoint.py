009c2a29ed10a7f040b815f3a775ebdf9ec0db96
kaiqueBellmont <kaiquebellmont@gmail.com>
2023-04-27 17:45:39 -0300

    Teste

---
+++ chckpoint.py
@@ -0,0 +1,56 @@
+import subprocess
+import requests
+import json
+
+
+def evaluate_code_quality(code):
+    """
+    :param code:
+    :return:
+    """
+    # Dados a serem enviados para a API
+    data = {
+        'code': code
+    }
+
+    # Fazer a chamada para a API e enviar os dados
+    api_url = "http://exemplo.com/api/avaliacao"
+    response = requests.post(api_url, json=data)
+
+    # Verificar se a chamada foi bem-sucedida
+    if response.status_code == 200:
+        # Converter a resposta JSON em um dicionário Python
+        result = json.loads(response.text)
+
+        # Processar os resultados da avaliação
+        # ... faça o que for necessário com os resultados ...
+
+        # Retornar True ou False, dependendo do resultado da avaliação
+        return True
+    else:
+        # print("Falha ao chamar a API:", response.status_code)
+        print("Seu codigo nao segue a pep8:", response.status_code)
+        return False
+
+
+if __name__ == "__main__":
+    try:
+        # Capturar as mudanças em estágio (staged changes) usando o comando git diff
+        output = subprocess.check_output(['git', 'diff', '--staged', '--name-only'], universal_newlines=True)
+        # Filtrar apenas os arquivos .py modificados
+        arquivos_modificados = [arquivo for arquivo in output.split('\n') if arquivo.endswith('.py')]
+        print(arquivos_modificados)
+        # Loop pelos arquivos modificados
+        for arquivo in arquivos_modificados:
+            try:
+                with open(arquivo, 'r') as file:
+                    code = file.read()
+                    print("Alterações no arquivo:", arquivo)
+                    print(code)
+
+            except IOError as e:
+                print("Erro ao abrir o arquivo:", e)
+                exit(1)
+    except subprocess.CalledProcessError as e:
+        print("Erro ao capturar as mudanças em estágio:", e)
+        exit(1)
---
+++ cli.py
@@ -0,0 +1,33 @@
+import os
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def create_pre_commit():
+    pre_commit_script = """#!/bin/sh\npython3 -c "import script"""
+
+
+    hooks_dir = os.path.join(".git", "hooks")
+    pre_commit_path = os.path.join(hooks_dir, "pre-commit")
+
+    # Verificar se o diretório .git/hooks existe
+    if not os.path.exists(hooks_dir):
+        os.makedirs(hooks_dir)
+
+    # Criar o arquivo .git/hooks/pre-commit
+    with open(pre_commit_path, "w") as file:
+        file.write(pre_commit_script)
+
+    # Dar permissão de execução ao arquivo
+    os.chmod(pre_commit_path, 0o755)
+
+    print("Hook pre-commit criado com sucesso!")
+
+
+if __name__ == "__main__":
+    cli()
